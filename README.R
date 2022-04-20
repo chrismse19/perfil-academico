1e5128e7879e5be85b3e7fb3ab190a12a820c588
chrismse19 <103953476+chrismse19@users.noreply.github.com>
2022-04-20 15:00:11 -0600

    Actualizacion

--- README.R
+++ README.R
@@ -1,4 +1,5 @@
 # Perfil academico Christopher Cadena
 
-Estudiante de Geografia de la Universidad de Costa Rica
+- Estudiante de la Geografia de la Universidad de Costa Rica
+- Estudiante del programa de Pregrado
 
